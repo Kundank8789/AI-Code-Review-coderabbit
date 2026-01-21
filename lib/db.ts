cac57bfd4ee86399880a096250a0f3c25f91cc4b
kundan <kundan8789@gmail.com>
2026-01-21 14:26:03 +0530

    Fix prisma + polar client for vercel

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,7 @@ import {PrismaPg} from "@prisma/adapter-pg";
 
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL 
+    connectionString: process.env.DATABASE_URL!,
 })
 
 const prismaClientSingleton =()=>{
